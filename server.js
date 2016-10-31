89c178e211b719c63288360bdbd88b9882ff6bb5
mckconor <mckennc9@tcd.ie>
2016-10-31 21:33:17 +0000

    Demo

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 
 //Port No.
 var port = process.env.PORT || 8080;
+var cool = require('cool-faces-ascii');
 
 //EJS, like HTML but not for some reason
 app.set('view engine', 'ejs');
@@ -15,6 +16,10 @@ app.get('/', function(request, response){
 	//ejs render looks in view folder automagically
 	response.render('index');
 });
+
+app.get('/ascii', function(request, response){
+	response.send(cool());
+})
 
 app.listen(port, function(){
 	console.log('App running on http://localhost:' + port);
